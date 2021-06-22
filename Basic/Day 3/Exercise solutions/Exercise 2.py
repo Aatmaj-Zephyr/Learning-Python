946c991f401a8dc6957b8bfbfb0aa99064241193
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-06-22 13:12:34 +0530

    Added a comment.

--- Basic/Day 3/Exercise solutions/Exercise 2.py
+++ Basic/Day 3/Exercise solutions/Exercise 2.py
@@ -1,6 +1,6 @@
 a=int(input("Enter a number "))
 if a>0:
- if a%2==0:
+ if a%2==0: #the % operator returns remainder when divided by 2.
   print(a," is a positive even number")
  else:
   print(a," is a positive odd number")
