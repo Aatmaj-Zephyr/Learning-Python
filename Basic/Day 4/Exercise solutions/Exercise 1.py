e0494fc1a84bf0b2cf7abd6ecb641e83989073fb
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-06-23 21:55:33 +0530

    Update Exercise 1.py

--- Basic/Day 4/Exercise solutions/Exercise 1.py
+++ Basic/Day 4/Exercise solutions/Exercise 1.py
@@ -1,6 +1,6 @@
 #bug one- the range of the for loop must begin from one and not zero.
 # bug two- the range of the while loop must end with n+1 and not n
-n=int(input("Enter a number"))
+n=int(input("Enter a number "))
 for i in range (1,n+1):
   if n%i==0:
     print(i)
