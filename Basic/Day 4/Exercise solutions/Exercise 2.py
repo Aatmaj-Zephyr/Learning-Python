8b8500aec4dac16832889b800101ec1324567266
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-06-23 21:59:27 +0530

    Create Exercise 2.py

---
+++ Basic/Day 4/Exercise solutions/Exercise 2.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number"))
+s=0
+count=0
+for i in range (1,n+1):
+  if n%i==0:
+    print(i)
+    s+=i
+    count+=1
+print("Sum of the factors = ",s)
+print("Count of the factors = ",count)
