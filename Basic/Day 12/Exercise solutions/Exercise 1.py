41513a0e788816138a8f15728bbef4d938857cf2
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-07-08 10:29:25 +0530

    Create Exercise 1.py

---
+++ Basic/Day 12/Exercise solutions/Exercise 1.py
@@ -0,0 +1,16 @@
+a=[1,3,4,6,5,2,6]
+Bool=False
+n=int(input("Please enter the number to be searched "))
+#Method-1
+for i in range(len(a)):
+    if (a[i]==n):
+        print("Method-1 Yes, the number is in the list ")
+        Bool=True
+        break
+#Method-2
+if(n in a):
+    print("Method-2 Yes, the number is in the list ")
+    Bool=True
+    
+if(not Bool):
+    print("Not in list")
