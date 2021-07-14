6af7d647b37d5360a3a4d118fc8bf3a6abc937a2
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-07-14 10:13:09 +0530

    Create Sample question1.py

---
+++ Basic/Day 16/Sample questions/Sample question1.py
@@ -0,0 +1,9 @@
+a=[]
+for i in range(0,4):
+    a.append(ord(input("Please enter a character ")))
+a=sorted(a)
+#The sorted() method sorts the list and returns the sorted list
+# This is an inbuilt function to sort the list. You can also use insertion sort.
+for i in range(0,len(a)):
+    a[i]=chr(a[i])
+print(a)
