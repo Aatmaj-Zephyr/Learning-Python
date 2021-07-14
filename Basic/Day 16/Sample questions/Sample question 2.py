0c5f0a526e0a94f5889c9cb03554217bb7a6f69d
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-07-14 10:54:33 +0530

    Update Sample question 2.py

--- Basic/Day 16/Sample questions/Sample question 2.py
+++ Basic/Day 16/Sample questions/Sample question 2.py
@@ -2,5 +2,7 @@ a=ord(input("Please enter a letter "))
 if(65<=a<=65+26): #65=A
     print(chr(a-65+97))
     #65=A, 97=a 
-if(97<=a<=97+26): #65=A
+elif(97<=a<=97+26): #65=A
     print(chr(a+65-97))
+else:
+    print("Error. Please enter only characters.")
