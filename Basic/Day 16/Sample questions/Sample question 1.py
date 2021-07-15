79b610d1f9d5f975b4926f344901bee257124d4d
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-07-15 10:58:18 +0530

    Update Sample question 1.py

--- Basic/Day 16/Sample questions/Sample question 1.py
+++ Basic/Day 16/Sample questions/Sample question 1.py
@@ -1,6 +1,9 @@
 a=[]
 for i in range(0,4):
+   try:
     a.append(ord(input("Please enter a character ")))
+   except:
+    print("Error!")
 a=sorted(a)
 #The sorted() method sorts the list and returns the sorted list
 # This is an inbuilt function to sort the list. You can also use insertion sort.
