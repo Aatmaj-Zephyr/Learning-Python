04bcbd5f0b103ee416be71beee5d305025162517
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-19 12:55:16 +0530

    Update Example 5.py

--- Intermediate/Day 11/Sample examples/Example 5.py
+++ Intermediate/Day 11/Sample examples/Example 5.py
@@ -3,3 +3,6 @@ randomlist = []
 for i in range(0,5):
  randomlist.append(rd.randint(15,30))
 print(randomlist)
+'''
+The sample() method takes two arguments. One is the list of numbers to choose random numbers from, and the other is the number of random numbers to choose
+'''
