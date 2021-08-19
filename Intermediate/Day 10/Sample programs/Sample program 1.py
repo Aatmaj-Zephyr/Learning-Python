9a660dbd5f15f958b1cf51e719e79baf14446e5f
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-19 10:19:45 +0530

    Create Sample program 1.py

---
+++ Intermediate/Day 10/Sample programs/Sample program 1.py
@@ -0,0 +1,7 @@
+import cmath
+x = -1.0
+y = 0.0
+# converting x and y into complex number
+z = complex(x,y);
+# printing phase of a complex number using phase()
+print ("The phase of complex number is : ",cmath.phase(z))
