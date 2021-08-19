0a2834b0bbccaee154244323ed53a4afac3c59d1
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-19 12:57:22 +0530

    Update and rename Example  6.py to Example 6.py

--- Intermediate/Day 11/Sample examples/Example  6.py
+++
@@ -1,6 +0,0 @@
-import random as rd
-randomlist = rd.sample(range(15, 30), 5)
-print(randomlist)
-'''
-The sample() method takes two arguments. One is the list of numbers to choose random numbers from, and the other is the number of random numbers to choose
-'''
