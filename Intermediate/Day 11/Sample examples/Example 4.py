b0db05ea6ec5df1afedda944c2bb302c03197c8e
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-19 12:54:05 +0530

    Update Example 4.py

--- Intermediate/Day 11/Sample examples/Example 4.py
+++ Intermediate/Day 11/Sample examples/Example 4.py
@@ -1,5 +1,5 @@
 import random as rd
-#This is shorthand for writing the entire module name over and over again
+# This is shorthand for writing the entire module name over and over again
 for i in range(0,5):
- n = rd.randint(1,10)
+ n = rd.randint(1,10) # instead of random.randint() we write rd.randint()
  print(n)
