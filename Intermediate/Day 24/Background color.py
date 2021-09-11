b855bfbcff1f2d65d5a46a4c3f643201aebdeaf9
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-11 15:11:29 +0530

    Create Background color.py

---
+++ Intermediate/Day 24/Background color.py
@@ -0,0 +1,9 @@
+from tkinter import *
+master= Tk()
+master.title("Color Demo...")
+master.geometry("300x200")
+master.configure(bg='pINk') # Not case sensative
+
+a=Label(master,text="Welcome",bg="yellow")
+a.pack()
+master.mainloop()
