7a8f3c6f7f438971da404864a9da69c8f2c6218b
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-14 19:52:00 +0530

    Create Making a blank button.py

---
+++ Intermediate/Day 25/Making a blank button.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+form=tk.Tk()
+form.title("Example of Entry widget")
+form.geometry('400x200')
+
+button=tk.Button(form,text="Press Me")
+button.pack()
+form.mainloop()
+show()
