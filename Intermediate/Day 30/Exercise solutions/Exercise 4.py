3b1c96cccc1a0feef1cf62a44123b891b3ba0c18
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-22 10:58:39 +0530

    Create Exercise 4.py

---
+++ Intermediate/Day 30/Exercise solutions/Exercise 4.py
@@ -0,0 +1,15 @@
+from tkinter import *
+frame = Tk()
+frame.geometry("200x200")
+items=("Python","R","Julia","MATLAB","Mathematica","Haskell")
+Lb = Listbox(frame,height=len(items))
+for i in range(0,len(items)):
+    Lb.insert(i,items[i])
+
+Lb.pack()
+def show():
+    showbutton.config(text=items[Lb.curselection()[0]])
+showbutton=Button(frame,text="show",command=show)
+showbutton.pack()
+mainloop()
+frame.mainloop()
