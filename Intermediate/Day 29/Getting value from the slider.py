b270a123ae599003b26e04c0aab617ed940f2426
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-22 11:21:22 +0530

    Create Getting value from the slider.py

---
+++ Intermediate/Day 29/Getting value from the slider.py
@@ -0,0 +1,11 @@
+from tkinter import *
+frame=Tk()
+frame.geometry("200x200")
+slider=Scale(frame,from_=0, to=10, orient="horizontal")
+slider.set(2)
+slider.pack()
+def reset():
+    resetbutton.config(text=slider.get())
+resetbutton=Button(frame,text="show",command=reset)
+resetbutton.pack()
+mainloop()
