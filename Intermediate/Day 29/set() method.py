b092426615d83b9b67cb0f9f5b5af8912456c716
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-22 11:20:08 +0530

    Create set() method.py

---
+++ Intermediate/Day 29/set() method.py
@@ -0,0 +1,11 @@
+from tkinter import *
+frame=Tk()
+frame.geometry("200x200")
+slider=Scale(frame, from_=0, to=10)
+slider.set(2)
+slider.pack()
+def reset():
+    slider.set(2)
+resetbutton=Button(frame, text="reset",command=reset)
+resetbutton.pack()
+mainloop()
