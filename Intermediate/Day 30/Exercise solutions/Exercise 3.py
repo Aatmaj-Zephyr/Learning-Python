9224714f763be2e4713ff5a9f900a4b63760e5b2
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-22 10:56:32 +0530

    Create Exercise 3.py

---
+++ Intermediate/Day 30/Exercise solutions/Exercise 3.py
@@ -0,0 +1,19 @@
+from tkinter import *
+frame = Tk()
+frame.geometry("200x200")
+
+Lb = Listbox(frame)
+Lb.insert(1, "Python")
+Lb.insert(2, "R")
+Lb.insert(3, "Julia")
+Lb.insert(4, "MATLAB")
+Lb.insert(5, "Mathematica")
+Lb.insert(6, "Haskell")
+
+Lb.pack()
+def show():
+    showbutton.config(text=Lb.get(Lb.curselection()))
+showbutton=Button(frame,text="show",command=show)
+showbutton.pack()
+mainloop()
+frame.mainloop()
