4ac2afd5dcd68a193db6e2d6c739a1c607aa228a
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-23 10:13:05 +0530

    Create Multiple selection.py

---
+++ Intermediate/Day 30/Multiple selection.py
@@ -0,0 +1,12 @@
+from tkinter import *
+frame = Tk()
+frame.geometry("200x200")
+items=("Python","R","Julia","MATLAB","Mathematica","Haskell")
+Lb = Listbox(frame,selectmode=MULTIPLE)
+for i in range(0,len(items)):
+    Lb.insert(i,items[i])
+
+Lb.pack()
+
+mainloop()
+frame.mainloop()
